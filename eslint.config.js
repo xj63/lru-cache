b017547bf570eaefaa716b133ebb26e71029b2eb
xj63 <xj63work@163.com>
2025-07-17 15:27:48 +0800

    chore: update lint config to allow _p unused var.

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,24 @@
 import { sxzz } from '@sxzz/eslint-config'
 
-export default sxzz()
+export default sxzz(
+  {
+    // Features: it'll detect installed dependency and enable necessary features automatically
+  },
+  [
+    /* custom config */
+    {
+      rules: {
+        // enable _p var not use
+        'unused-imports/no-unused-vars': [
+          'warn',
+          {
+            vars: 'all',
+            varsIgnorePattern: '^_',
+            args: 'after-used',
+            argsIgnorePattern: '^_',
+          },
+        ],
+      },
+    },
+  ],
+)
